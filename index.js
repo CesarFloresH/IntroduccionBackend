c1d7e695f42925f9d96169400b6d4240c180c914
CesarFloresH <cesar25.566@gmail.com>
2022-04-26 13:38:16 -0600

    Se realizo un cambio en el index en donde el texto es html, ademas el "Hola Mundo" se muestra como una etiqueta y en package cambio la version.

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const hostname = '192.168.50.123';
 const port = 3000;
 const server = http.createServer((req,res) => {
     res.statuscode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hola Mundo\n');
+    res.setHeader('Content-Type','text/html');
+    res.end('<h1>Hola Mundo</h1>');
+    
 } );
 server.listen(port,hostname,() =>{
     console.log("el servidor se esta ejecuntando en http://${hostname}:${port}/");

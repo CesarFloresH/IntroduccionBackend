da83354b8b2acdc781b1b0cebe02a54fa2d4fe67
CesarFloresH <cesar25.566@gmail.com>
2022-05-02 14:46:22 -0600

    Se realizo cambios al archivo servidor.js, al index.html y se modifico la version del package.json por los cambios que se realizo.

--- servidor.js
+++ servidor.js
@@ -6,11 +6,12 @@ http.createServer(function(request, response){
     console.log('request', request.url);
     var filePath = '.' + request.url;
     if(filePath == './'){
-        filePath = './index.html';
+        filePath = "./index.html";
     }
     var extname = String(path.extname(filePath)).toLowerCase();
     var contentType = 'text/html';
     var mimeTypes = {
+        '.html' : 'text/html', 
         '.js' : 'text/javascript',
         '.css' : 'text/css',
         '.jpg' : 'image/jpg'
@@ -18,9 +19,9 @@ http.createServer(function(request, response){
     contentType = mimeTypes[extname] || 'application/octet-stream';
     fs.readFile(filePath, function(error, content){
         if(error){
-            if(error.code == 'ENDENT'){
+            if(error.code == 'ENOENT'){
                 fs.readFile('./404.html',function(error,content){
-                   response.writeHead(200,{'Content-Type':contentType});
+                   response.writeHead(200,{'Content-Type': contentType});
                    response.end(content, 'utf-8'); 
                 });
             }
